64c8a9761c8fcc88453f8b4577e7ff65220f90ab
saifkaral <101649926+saifkaral@users.noreply.github.com>
2025-09-14 23:38:50 +0300

    completed the problem

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(String,times) {
+    let newstr = ""
+    if (times < 0) {
+        return "ERROR"
+    }
+    else {
+        for (let i = 0; i<times;i++) {
+            newstr+=String;
+        }
+    }
+    
+    return newstr;
 };
 
 // Do not edit below this line
