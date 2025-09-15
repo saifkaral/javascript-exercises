95f842d2909512b1a9e3f5153cc5a50394e99d93
saifkaral <101649926+saifkaral@users.noreply.github.com>
2025-09-15 13:20:10 +0300

    solved the problem in a bit of a weird way

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let arg of args) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arg) {
+                delete (array[i]);
+            }
+        }
+    }
+    function filtering (value) {
+        return value != undefined;
+    }
+    return (array.filter(filtering));
 };
 
 // Do not edit below this line
