e281bcb09b17bf4a83be3c346739b9d43cd80459
saifkaral <101649926+saifkaral@users.noreply.github.com>
2025-09-15 18:04:08 +0300

    solved the problem

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temprature) {
+  if (Number.isInteger(((temprature-32)*(5/9)))) {
+    return ((temprature-32)*(5/9));
+  }
+  else {
+    return parseFloat(((temprature-32)*(5/9)).toFixed(1));
+  }
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temprature) {
+  if (Number.isInteger(((temprature*9/5)+32))) {
+    return ((temprature*9/5)+32)
+  }
+  else {
+    return parseFloat(((temprature*9/5)+32).toFixed(1));
+  }
+  
 };
 
 // Do not edit below this line
